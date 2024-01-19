0f2f903862abd12a6d3331da4e41dac9115fa7bc
Arthur Willyams <106842143+r2WillDev@users.noreply.github.com>
2024-01-19 17:56:00 -0300

    otimizando o menu

--- Program.cs
+++ Program.cs
@@ -1,23 +1,34 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone+
 //FEITO
+while (true)
+{
 Console.WriteLine("QUAL CELULAR VOCE DESEJA USAR");
 Console.WriteLine("1 - Iphone");
 Console.WriteLine("2 - Nokia");
+Console.WriteLine("3 - Sair");
 
-switch (Console.ReadLine())
-{
-    case "1" :
-        Iphone.TestarIphone();
-        break;
+    switch (Console.ReadLine())
+    {
+        case "1":
+            Iphone.TestarIphone();
+            break;
 
-    case "2" :
-        Nokia.TestarNokia();
-        break;
+        case "2":
+            Nokia.TestarNokia();
+            break;
+        
+        case "3":
+            Console.WriteLine("Saindo do programa...");
+            return;
 
-    default:
-        Console.WriteLine("Opção invalida");
-        break;
+        default:
+            Console.WriteLine("Opção invalida");
+            break;
+    }
+
+    Console.WriteLine("Pressione qualquer tecla para continuar");
+    Console.ReadKey();
+    Console.Clear();
 }
-
